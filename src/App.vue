07e9e7df99eda144eae5bbf2749191b7c4b1a0f2
jmcbade <jmcbade@gmail.com>
2020-06-08 08:19:26 +0900

    Course at 47:37 UUID - Not sure what version of theuuid command to specify

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,72 @@
 <template>
-  <div id="app">
-<Todos v-bind:todos="todos"/>
-  </div>
+	<div id="app">
+      <Header />
+			<AddTodo />
+		<Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
+	</div>
 </template>
 
 <script>
-import Todos from './components/Todos'
-export default {
-  name: 'App',
-  components: {
-    Todos
-  },
-  data() {
-    return {
-      todos: [
-        {
-        id: 1,
-        title: "Todo One",
-        completed: false
-        },
-                {
-        id: 2,
-        title: "Todo Two",
-        completed: true
-        },
-                {
-        id: 3,
-        title: "Todo Three",
-        completed: false
-        },
-      ]
-    }
-  }
-}
+	import Header from './components/layout/Header';
+	import Todos from './components/Todos';
+	import AddTodo from "./components/AddTodo";
+
+	export default {
+		name: 'App',
+		components:  {
+			Header,
+			Todos,
+			AddTodo
+		},
+		data() {
+			return {
+				todos: [
+					{
+						id: 1,
+						title: "Todo One",
+						completed: false
+					},
+					{
+						id: 2,
+						title: "Todo Two",
+						completed: true
+					},
+					{
+						id: 3,
+						title: "Todo Three",
+						completed: false
+					},
+				]
+			}
+		},
+      methods: {
+          deleteTodo(id) {
+              this.todos = this.todos.filter(todo => todo.id !== id);
+          }
+      }
+	}
 </script>
 
 <style>
- * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+	* {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+	}
 
+	body {
+		font-family: Arial, Helvetica, sans-serif;
+		line-height: 1.4;
+	}
+	.btn {
+		display: inline-block;
+		border: none;
+		background: #555;
+		color: #fff;
+		padding: 7px 20px;
+		cursor: pointer;
+	}
+	.btn:hover {
+		background: #666;
+	}
 </style>
