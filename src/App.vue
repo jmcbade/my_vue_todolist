bc77ddfa9482232d90b516ee7fbb3d858451386b
jmcbade <jmcbade@gmail.com>
2020-06-09 17:13:56 +0900

    Updated to use json placeholder code nad added delete item code

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
 	<div id="app">
-      <Header />
-			<AddTodo v-on:add-todo="addTodo"/>
+		<Header/>
+		<AddTodo v-on:add-todo="addTodo"/>
 		<Todos v-bind:todos="todos" v-on:del-todo="deleteTodo"/>
 	</div>
 </template>
@@ -10,43 +10,43 @@
 	import Header from './components/layout/Header';
 	import Todos from './components/Todos';
 	import AddTodo from "./components/AddTodo";
+	import axios from 'axios';
 
 	export default {
 		name: 'App',
-		components:  {
+		components: {
 			Header,
 			Todos,
 			AddTodo
 		},
 		data() {
 			return {
-				todos: [
-					{
-						id: 1,
-						title: "Todo One",
-						completed: false
-					},
-					{
-						id: 2,
-						title: "Todo Two",
-						completed: true
-					},
-					{
-						id: 3,
-						title: "Todo Three",
-						completed: false
-					},
-				]
+				todos: []
 			}
 		},
-      methods: {
-					deleteTodo(id) {
-              this.todos = this.todos.filter(todo => todo.id !== id);
-          },
-          addTodo: function (newTodo) {
-          this.todos = [...this.todos, newTodo];
-        }
-      }
+		methods: {
+			deleteTodo(id) {
+				axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+					// eslint-disable-next-line no-unused-vars
+					.then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+					.catch(err => console.log(err));
+			},
+			addTodo: function (newTodo) {
+				const {title, completed} = newTodo;
+				axios.post('https://jsonplaceholder.typicode.com/todos', {
+						title,
+						completed
+					})
+					.then(res => this.todos = [...this.todos, res.data])
+					.catch(err => console.log(err));
+
+			}
+		},
+		created() {
+			axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+				.then(res => this.todos = res.data)
+				.catch(err => console.log(err));
+		}
 	}
 </script>
 
@@ -61,6 +61,7 @@
 		font-family: Arial, Helvetica, sans-serif;
 		line-height: 1.4;
 	}
+
 	.btn {
 		display: inline-block;
 		border: none;
@@ -69,6 +70,7 @@
 		padding: 7px 20px;
 		cursor: pointer;
 	}
+
 	.btn:hover {
 		background: #666;
 	}
